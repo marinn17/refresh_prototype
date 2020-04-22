be32af3c21d33eed732e8f4d15bdbb9cf8460aa9
marinn17 <cedillo.marinn@gmail.com>
2020-04-22 02:54:50 -0700

    filter css

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+var currentDay = new Date();
+document.getElementById("date").innerHTML = currentDay.toDateString();
+
+var hour = currentDay.getHours();
+
+if (hour >= 7 && hour < 10)
+  document.getElementById("mealTime").innerHTML = "Breakfast";
+else if (hour > 11 && hour <= 2) {
+  document.getElementById("mealTime").innerHTML = "Lunch";
+}
+else if (hour >5 && hour <= 7) {
+  document.getElementById("mealTime").innerHTML = "Dinner";
+}
+else {
+  document.getElementById("mealTime").innerHTML = "Closed";
+
+}
+
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -7,6 +25,7 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
 
 function changeLocation(value) {
     document.getElementById("location").innerHTML = value;
@@ -30,4 +49,59 @@ window.onclick = function(event) {
       }
     }
   }
+}
+
+
+
+
+
+
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("filterBtn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
