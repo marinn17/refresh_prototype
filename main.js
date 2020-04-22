e663bfd430d4434514189b00873713c7c09d1691
marinn17 <cedillo.marinn@gmail.com>
2020-04-22 00:37:41 -0700

    updated front page

---
+++ main.js
@@ -0,0 +1,33 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+function changeLocation(value) {
+    document.getElementById("location").innerHTML = value;
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function toggleLocationMenu() {
+  document.getElementById("locationDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
